354606429e2f65675582fe15fda19bdb559d34b6
Sahel <mahdieh.ashrafi.s.a@gmail.com>
2023-01-12 13:54:53 -0800

    admin panel structure

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminDashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| Admin Panel
+|--------------------------------------------------------------------------
+|
+|
+*/
+
+Route::prefix('admin')->namespace('Admin')->group(function (){
+    Route::get('/',[AdminDashboardController::class, 'index'])->name('admin.home');
+
 });
